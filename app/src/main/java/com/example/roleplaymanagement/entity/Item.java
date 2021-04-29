b518c029976a1829da3f7f9a77348e0905a71fff
DylanCampion <dylan.campion32@gmail.com>
2021-04-29 17:01:46 +0200

    ✨ added Item class

---
+++ app/src/main/java/com/example/roleplaymanagement/entity/Item.java
@@ -0,0 +1,11 @@
+package com.example.roleplaymanagement.entity;
+
+public class Item {
+    private String name;
+    private int weight;
+
+    public Item(String name, int weight) {
+        this.name = name;
+        this.weight = weight;
+    }
+}
