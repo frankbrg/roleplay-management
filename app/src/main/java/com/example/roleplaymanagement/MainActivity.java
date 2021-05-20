6e95e9de45d74cd5cc786b8124e7c70d9a31ce70
Frank Bergé <frank.brg@icloud.com>
2021-05-20 16:34:38 +0200

    :sparkles: added view adapter

--- app/src/main/java/com/example/roleplaymanagement/MainActivity.java
+++ app/src/main/java/com/example/roleplaymanagement/MainActivity.java
@@ -1,17 +1,28 @@
 package com.example.roleplaymanagement;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
-import com.example.roleplaymanagement.tests.Character;
+import com.example.roleplaymanagement.entity.Character;
+
+import com.example.roleplaymanagement.recycler.CardCharacterViewAdapter;
+
+import java.util.ArrayList;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    CardCharacterViewAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
+
 
         setContentView(R.layout.activity_edit_character);
 
@@ -21,5 +32,32 @@ public class MainActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_add_character);
 
+        setContentView(R.layout.activity_main);
+
+        // data to populate the RecyclerView with
+        ArrayList<Character> characters = new ArrayList<>();
+        characters.add(new Character("Gandalf", 40));
+        characters.add(new Character("Dylan", 10));
+        characters.add(new Character("Florian", 17));
+        characters.add(new Character("Frank", 14));
+        characters.add(new Character("Gandalf", 40));
+        characters.add(new Character("Dylan", 10));
+        characters.add(new Character("Florian", 17));
+        characters.add(new Character("Frank", 14));
+        characters.add(new Character("Gandalf", 40));
+        characters.add(new Character("Dylan", 10));
+        characters.add(new Character("Florian", 17));
+        characters.add(new Character("Frank", 14));
+        characters.add(new Character("Gandalf", 40));
+        characters.add(new Character("Dylan", 10));
+        characters.add(new Character("Florian", 17));
+        characters.add(new Character("Frank", 14));
+
+        // set up the RecyclerView
+        RecyclerView recyclerView = findViewById(R.id.rvCharacters);
+        recyclerView.setLayoutManager(new GridLayoutManager(this, 2));
+        adapter = new CardCharacterViewAdapter(characters);
+        recyclerView.setAdapter(adapter);
     }
+
 }
