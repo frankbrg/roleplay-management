84122fc04665c888a7aec061e31c5982d85a614c
Frank Bergé <frank.brg@icloud.com>
2021-05-20 10:03:19 +0200

    ✨ added engine for inventory

--- app/src/main/java/com/example/roleplaymanagement/entity/Character.java
+++ app/src/main/java/com/example/roleplaymanagement/entity/Character.java
@@ -1,12 +1,17 @@
 package com.example.roleplaymanagement.entity;
 
+import java.util.ArrayList;
+
 public class Character {
     private String name;
     private int inventorySize;
+    private int inventoryUsedSize;
+    private ArrayList<Item> items;
 
     public Character(String name, int inventorySize) {
         this.name = name;
         this.inventorySize = inventorySize;
+        this.items = new ArrayList<>();
     }
 
     public String getName() {
@@ -23,5 +28,26 @@ public class Character {
 
     public void setInventorySize(int inventorySize) {
         this.inventorySize = inventorySize;
+    }
+
+
+    public int getInventoryUsedSize() {
+        return inventoryUsedSize;
+    }
+
+    public void setInventoryUsedSize(int inventoryUsedSize) {
+        this.inventoryUsedSize = inventoryUsedSize;
+    }
+
+    public void addItem(Item item) {
+        this.items.add(item);
+    }
+
+    public ArrayList<Item> getItems() {
+        return items;
+    }
+
+    public void setItems(ArrayList<Item> items) {
+        this.items = items;
     }
 }
