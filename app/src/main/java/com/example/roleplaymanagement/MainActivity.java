bbba6e3341ee78ba83800ec3bb96d51d7176ba5e
DylanCampion <dylan.campion32@gmail.com>
2021-06-11 16:48:28 +0200

    :zap: button add character linked to add character page

--- app/src/main/java/com/example/roleplaymanagement/MainActivity.java
+++ app/src/main/java/com/example/roleplaymanagement/MainActivity.java
@@ -2,37 +2,61 @@ package com.example.roleplaymanagement;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.GridLayoutManager;
-import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.roleplaymanagement.entity.Character;
 
 import com.example.roleplaymanagement.recycler.CardCharacterViewAdapter;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.ArrayList;
 
-
 public class MainActivity extends AppCompatActivity {
+
+    private Button button;
+
 
     CardCharacterViewAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //setContentView(R.layout.activity_edit_character);
+
+       /* button = (Button) findViewById(R.id.floatingActionButton);
+
+        button.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                openActivity_add_character();
+            }
+        });*/
+
+        FloatingActionButton button = findViewById(R.id.floatingActionButton);
+        System.out.println(button);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ActivityAddCharacter.class);
+                startActivity(intent);
+                        }
+        });
 
 
-        setContentView(R.layout.activity_edit_character);
 
         Character character = new Character("fisty", 152);
 
         System.out.println(character);
 
-        setContentView(R.layout.activity_add_character);
+        //setContentView(R.layout.activity_add_character);
 
-        setContentView(R.layout.activity_main);
 
         // data to populate the RecyclerView with
         ArrayList<Character> characters = new ArrayList<>();
