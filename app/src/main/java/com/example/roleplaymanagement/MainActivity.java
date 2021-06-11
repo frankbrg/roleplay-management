4177cd14a4d3fc681f72e5d57e4d64d9d9f653d9
Fant1p <fantin.piccoli@gmail.com>
2021-06-11 14:50:32 +0200

    ajout ID dans character

--- app/src/main/java/com/example/roleplaymanagement/MainActivity.java
+++ app/src/main/java/com/example/roleplaymanagement/MainActivity.java
@@ -6,6 +6,7 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.LinearLayout;
 
 import com.example.roleplaymanagement.entity.Character;
 
@@ -23,10 +24,9 @@ public class MainActivity extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
 
-
         setContentView(R.layout.activity_edit_character);
 
-        Character character = new Character("fisty", 152);
+        Character character = new Character(0,"fisty", 152);
 
         System.out.println(character);
 
@@ -36,22 +36,22 @@ public class MainActivity extends AppCompatActivity {
 
         // data to populate the RecyclerView with
         ArrayList<Character> characters = new ArrayList<>();
-        characters.add(new Character("Gandalf", 40));
-        characters.add(new Character("Dylan", 10));
-        characters.add(new Character("Florian", 17));
-        characters.add(new Character("Frank", 14));
-        characters.add(new Character("Gandalf", 40));
-        characters.add(new Character("Dylan", 10));
-        characters.add(new Character("Florian", 17));
-        characters.add(new Character("Frank", 14));
-        characters.add(new Character("Gandalf", 40));
-        characters.add(new Character("Dylan", 10));
-        characters.add(new Character("Florian", 17));
-        characters.add(new Character("Frank", 14));
-        characters.add(new Character("Gandalf", 40));
-        characters.add(new Character("Dylan", 10));
-        characters.add(new Character("Florian", 17));
-        characters.add(new Character("Frank", 14));
+        characters.add(new Character(1,"Gandalf", 40));
+        characters.add(new Character(2,"Dylan", 10));
+        characters.add(new Character(3,"Florian", 17));
+        characters.add(new Character(4,"Frank", 14));
+        characters.add(new Character(5,"Gandalf", 40));
+        characters.add(new Character(6,"Dylan", 10));
+        characters.add(new Character(7,"Florian", 17));
+        characters.add(new Character(8,"Frank", 14));
+        characters.add(new Character(9,"Gandalf", 40));
+        characters.add(new Character(10,"Dylan", 10));
+        characters.add(new Character(11,"Florian", 17));
+        characters.add(new Character(12,"Frank", 14));
+        characters.add(new Character(13,"Gandalf", 40));
+        characters.add(new Character(14,"Dylan", 10));
+        characters.add(new Character(15,"Florian", 17));
+        characters.add(new Character(16,"Frank", 14));
 
         // set up the RecyclerView
         RecyclerView recyclerView = findViewById(R.id.rvCharacters);
--- app/src/main/java/com/example/roleplaymanagement/entity/Character.java
+++ app/src/main/java/com/example/roleplaymanagement/entity/Character.java
@@ -3,15 +3,24 @@ package com.example.roleplaymanagement.entity;
 import java.util.ArrayList;
 
 public class Character {
+    private int id;
     private String name;
     private int inventorySize;
     private int inventoryUsedSize;
     private ArrayList<Item> items;
 
-    public Character(String name, int inventorySize) {
+    public Character(int id, String name, int inventorySize) {
+        this.id = id;
         this.name = name;
         this.inventorySize = inventorySize;
         this.items = new ArrayList<>();
+    }
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 
     public String getName() {
