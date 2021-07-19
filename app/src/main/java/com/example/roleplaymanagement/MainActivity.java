513a0e2e1fd45e8201e6c68cded2b3bb7eeab4bc
Frank Bergé <frank.brg@icloud.com>
2021-07-19 15:49:18 +0200

    feat (adapter) : add item adapter

--- app/src/main/java/com/example/roleplaymanagement/MainActivity.java
+++ app/src/main/java/com/example/roleplaymanagement/MainActivity.java
@@ -11,7 +11,9 @@ import android.widget.Button;
 
 import com.example.roleplaymanagement.entity.Character;
 
+import com.example.roleplaymanagement.entity.Item;
 import com.example.roleplaymanagement.recycler.CardCharacterViewAdapter;
+import com.example.roleplaymanagement.recycler.CardItemViewAdapter;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.ArrayList;
@@ -21,12 +23,12 @@ public class MainActivity extends AppCompatActivity {
     private Button button;
 
 
-    CardCharacterViewAdapter adapter;
+    CardItemViewAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_list_item);
 
         //setContentView(R.layout.activity_edit_character);
 
@@ -40,19 +42,6 @@ public class MainActivity extends AppCompatActivity {
             }
         });*/
 
-        FloatingActionButton button = findViewById(R.id.floatingActionButton);
-        System.out.println(button);
-        button.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, ActivityAddCharacter.class);
-                startActivity(intent);
-                        }
-        });
-
-
-        Character character = new Character("fisty", 152);
-
-        System.out.println(character);
 
         //setContentView(R.layout.activity_add_character);
 
@@ -76,10 +65,33 @@ public class MainActivity extends AppCompatActivity {
         characters.add(new Character("Florian", 17));
         characters.add(new Character("Frank", 14));
 
+
+        ArrayList<Item> items = new ArrayList<>();
+        items.add(new Item("Sword", 7));
+        items.add(new Item("Sword", 7));
+        items.add(new Item("Sword", 7));
+        items.add(new Item("Sword", 7));
+        items.add(new Item("Sword", 7));
+        items.add(new Item("Sword", 7));
+        items.add(new Item("Sword", 7));
+        items.add(new Item("Sword", 7));
+        items.add(new Item("Sword", 7));
+        items.add(new Item("Sword", 7));
+        items.add(new Item("Sword", 7));
+        items.add(new Item("Sword", 7));
+        items.add(new Item("Sword", 7));
+        items.add(new Item("Sword", 7));
+        items.add(new Item("Sword", 7));
+        items.add(new Item("Sword", 7));
+
+        characters.get(0).setItems(
+                items
+        );
+
         // set up the RecyclerView
-        RecyclerView recyclerView = findViewById(R.id.rvCharacters);
+        RecyclerView recyclerView = findViewById(R.id.rvItems);
         recyclerView.setLayoutManager(new GridLayoutManager(this, 2));
-        adapter = new CardCharacterViewAdapter(characters);
+        adapter = new CardItemViewAdapter(items);
         recyclerView.setAdapter(adapter);
     }
 
---
+++ app/src/main/java/com/example/roleplaymanagement/recycler/CardItemViewAdapter.java
@@ -0,0 +1,80 @@
+package com.example.roleplaymanagement.recycler;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.roleplaymanagement.R;
+import com.example.roleplaymanagement.entity.Item;
+
+import java.util.ArrayList;
+
+public class CardItemViewAdapter extends RecyclerView.Adapter<CardItemViewAdapter.ViewHolder> {
+
+    private ArrayList<Item> localDataSet;
+
+    /**
+     * Provide a reference to the type of views that you are using
+     * (custom ViewHolder).
+     */
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        private final TextView textView;
+        private final TextView weight;
+
+        public ViewHolder(View view) {
+            super(view);
+            // Define click listener for the ViewHolder's View
+
+            textView = (TextView) view.findViewById(R.id.name);
+            weight = (TextView) view.findViewById(R.id.weight);
+        }
+        public TextView getTextView() {
+            return textView;
+        }
+        public TextView getWeight() {
+            return weight;
+        }
+
+    }
+
+    /**
+     * Initialize the dataset of the Adapter.
+     *
+     * @param dataSet String[] containing the data to populate views to be used
+     * by RecyclerView.
+     */
+    public CardItemViewAdapter(ArrayList<Item> dataSet) {
+        localDataSet = dataSet;
+    }
+
+    // Create new views (invoked by the layout manager)
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
+        // Create a new view, which defines the UI of the list item
+        View view = LayoutInflater.from(viewGroup.getContext())
+                .inflate(R.layout.recycler_view_item_object, viewGroup, false);
+
+        return new ViewHolder(view);
+    }
+
+    // Replace the contents of a view (invoked by the layout manager)
+    @Override
+    public void onBindViewHolder(ViewHolder viewHolder, final int position) {
+
+        // Get element from your dataset at this position and replace the
+        // contents of the view with that element
+        viewHolder.getTextView().setText(localDataSet.get(position).getName());
+        viewHolder.getWeight().setText(String.valueOf(localDataSet.get(position).getWeight()));
+
+    }
+
+    // Return the size of your dataset (invoked by the layout manager)
+    @Override
+    public int getItemCount() {
+        return localDataSet.size();
+    }
+}
+
